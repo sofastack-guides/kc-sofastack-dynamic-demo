383b222113077faf9782dee56e0d66e478bf6b96
guolei.sgl <guolei.sgl@antfin.com>
2020-04-03 10:58:36 +0800

    remove tracer

--- dynamic-stock-mng/src/main/java/io/sofastack/stockmng/controller/SortedController.java
+++ dynamic-stock-mng/src/main/java/io/sofastack/stockmng/controller/SortedController.java
@@ -2,10 +2,7 @@ package io.sofastack.stockmng.controller;
 
 
 import com.alipay.sofa.runtime.api.annotation.SofaReference;
-import com.alipay.sofa.tracer.plugin.flexible.annotations.Tracer;
-import com.alipay.common.tracer.core.tags.SpanTags;
 import io.sofastack.dynamic.facade.StrategyService;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -20,15 +17,9 @@ public class SortedController {
     @SofaReference
     private StrategyService strategyService;
 
-    @Autowired
-    io.opentracing.Tracer tracer;
-
     @RequestMapping("/")
-    @Tracer
     public String index(Model model) {
         try {
-
-            SpanTags.putTags("","");
             model.addAttribute("name", strategyService.strategy());
         }catch (Throwable ex){
             model.addAttribute("name", "DEFAULT BIZ");
